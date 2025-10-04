41b565fd608b77c4ab190fdc3430c919d40e503e
ALEXIS D. DELA CRUZ <addcx1developer@gmail.com>
2025-10-04 16:42:53 +0800

    Feat add save user

--- internal/repositories/user.go
+++ internal/repositories/user.go
@@ -1,6 +1,11 @@
 package repositories
 
-import "database/sql"
+import (
+	"database/sql"
+
+	"github.com/addcx1developer/event-booking-go-react/internal/models"
+	"github.com/addcx1developer/event-booking-go-react/internal/utils"
+)
 
 type UserStore struct {
 	db *sql.DB
@@ -10,4 +15,37 @@ func NewUserStore(db *sql.DB) *UserStore {
 	return &UserStore{
 		db: db,
 	}
+}
+
+func (s *UserStore) Save(u *models.User) error {
+	query := `
+		INSERT INTO users (email, password)
+		VALUES (?, ?)
+	`
+
+	hashedPassword, err := utils.HashedPassword(u.Password)
+	if err != nil {
+		return err
+	}
+
+	stmt, err := s.db.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	result, err := stmt.Exec(u.Email, hashedPassword)
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	u.ID = id
+
+	return nil
 }
