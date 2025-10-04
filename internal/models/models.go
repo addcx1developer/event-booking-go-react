ad8762169bcf9456185a1db24c7a84b9fd8d9614
ALEXIS D. DELA CRUZ <addcx1developer@gmail.com>
2025-10-04 13:17:37 +0800

    Feat add save event

--- internal/models/models.go
+++ internal/models/models.go
@@ -10,3 +10,9 @@ type Event struct {
 	DateTime    time.Time `json:"date_time"`
 	UserID      int64     `json:"user_id"`
 }
+
+type User struct {
+	ID       int64  `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
--- internal/repositories/event.go
+++ internal/repositories/event.go
@@ -16,4 +16,30 @@ func New(db *sql.DB) *EventStore {
 	}
 }
 
-func (s *EventStore) Save(e *models.Event) {}
+func (s *EventStore) Save(e *models.Event) error {
+	query := `
+		INSERT INTO events (name, description, location, date_time, user_id)
+		VALUES (?, ?, ?, ?, ?)
+	`
+
+	stmt, err := s.db.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	e.ID = id
+
+	return nil
+}
