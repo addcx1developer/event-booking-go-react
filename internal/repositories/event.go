8f9c70abebd045a8242709041764f43492e9b8d2
ALEXIS D. DELA CRUZ <addcx1developer@gmail.com>
2025-10-04 12:32:37 +0800

    Feat add create new event store

--- internal/repositories/event.go
+++ internal/repositories/event.go
@@ -1,1 +1,19 @@
 package repositories
+
+import (
+	"database/sql"
+
+	"github.com/addcx1developer/event-booking-go-react/internal/models"
+)
+
+type EventStore struct {
+	db *sql.DB
+}
+
+func New(db *sql.DB) *EventStore {
+	return &EventStore{
+		db: db,
+	}
+}
+
+func (s *EventStore) Save(e *models.Event) {}
